2bf98aeb904d7797e131e93ebc37bcbd864e1786
vinip1250-art <vinip1250@gmail.com>
2026-02-19 12:21:18 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  eslint:      { ignoreDuringBuilds: true },
-  typescript:  { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 }
 
 module.exports = nextConfig
